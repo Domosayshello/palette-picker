4179eddb84b8089044810fa481122ef2658b6a05
domosayshello <domininqiue.arno@gmail.com>
2023-11-01 12:40:13 -0400

    grabbing form data

--- src/main.js
+++ src/main.js
@@ -3,3 +3,22 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { getPalettes, setPalettes, initPalettesIfEmpty, addPalette, removePalette } from './data-store';
 
+// const displayPalette = () =>{
+
+// };
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target)
+    const formData = new FormData(event.target);
+    const { paletteTitle, color1, color2, color3, temp } = Object.fromEntries(formData);
+    form.reset();
+ };
+
+const main = () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
+};
+
+main();
+
